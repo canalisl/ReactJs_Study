494f876f70bdbcc55807e6656fb9f0ffd2f313b3
bruno <bruno95@naver.com>
2022-01-24 02:19:22 +0900

    modified

--- self-react-app/src/MovieApp.js
+++ self-react-app/src/MovieApp.js
@@ -4,7 +4,7 @@ import Detail from "./routes/Detail";
 
 function MovieApp() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         {/* 디테일의 useParams로 변수 값(id)를 넘겨줌 */}
