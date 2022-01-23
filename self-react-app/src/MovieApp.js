0d874644391d75c001493e22db4fb75f5eaf3d83
bruno <bruno95@naver.com>
2022-01-24 02:34:00 +0900

    MovieApp Route component path modified

--- self-react-app/src/MovieApp.js
+++ self-react-app/src/MovieApp.js
@@ -4,11 +4,14 @@ import Detail from "./routes/Detail";
 
 function MovieApp() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
         {/* 디테일의 useParams로 변수 값(id)를 넘겨줌 */}
-        <Route path="/movie/:id" element={<Detail />} />
+        <Route
+          path={`${process.env.PUBLIC_URL}/movie/:id`}
+          element={<Detail />}
+        />
       </Routes>
     </BrowserRouter>
   );
--- self-react-app/src/routes/Detail.js
+++ self-react-app/src/routes/Detail.js
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react/cjs/react.development";
 
 function Detail() {
